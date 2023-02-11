60a008a6a27afb1f33e91ff30f4d83adacc196b6
joshlaudone <joshlaudone@gmail.com>
2023-02-10 21:01:01 -0500

    Initial prototype of tuner widget + material UI

--- polyphonic_guitar_tuner/ui/TuningGUI.py
+++ polyphonic_guitar_tuner/ui/TuningGUI.py
@@ -1,13 +1,21 @@
-import kivy
-from kivy.app import App
+from kivymd.app import MDApp
+from kivymd.uix.widget import MDWidget
 from kivy.config import ConfigParser
 from kivy.lang import Builder
+from kivy.properties import NumericProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.settings import SettingsWithSidebar
 
 import os.path
 
 import settings_io.SettingsJson as SettingsJson
+
+class VerticalTuner(MDWidget):
+    circle_radius = NumericProperty(20)
+    circle_thickness = NumericProperty(20)
+    inner_circle_radius = NumericProperty(16)
+    cent_difference = NumericProperty(0)
+    cent_threshold = NumericProperty(5)
 
 class TunerWindow(Screen):
     pass
@@ -16,13 +24,15 @@ class WindowManager(ScreenManager):
     pass
 
 kv_file = os.path.join("ui", "Tuner.kv")
-kv = Builder.load_file(kv_file)
 
-class TuningGUI(App):
+class TuningGUI(MDApp):
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "LightBlue"
+        self.theme_cls.accent_palette = "Red"
         self.use_kivy_settings = False
         self.settings_cls = SettingsWithSidebar
-        return kv
+        return Builder.load_file(kv_file)
 
     def build_config(self, config):
         config.setdefaults('Tuning Settings', {
